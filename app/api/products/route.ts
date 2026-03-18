b6b4bec03902ee381e058583779d603f0a8ec2f5
robin <hossainmdrobin967@gmail.com>
2026-03-18 08:45:06 +0600

    sdfasd

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/lib/db';
 import Product from '@/models/Product';
 import Category from '@/models/Category';
+import '@/models/User'
+
 
 export async function GET(req: NextRequest) {
   try {
